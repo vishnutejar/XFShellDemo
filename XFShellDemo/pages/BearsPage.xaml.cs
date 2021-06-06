f3978a250e69c385ddf62a5fa51c6c99eb65d8dc
vishnu <vishnudevxamarin.gmail.com>
2021-06-06 21:44:19 +0530

    added bear details and cat details and list of data

--- XFShellDemo/pages/BearsPage.xaml.cs
+++ XFShellDemo/pages/BearsPage.xaml.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using XFShellDemo.Date;
 
 namespace XFShellDemo.pages
 {
@@ -15,6 +16,15 @@ namespace XFShellDemo.pages
         public BearsPage()
         {
             InitializeComponent();
+        }
+
+        async void OnCollectionViewSelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            string bearName = (e.CurrentSelection.FirstOrDefault() as Animal).Name;
+            // The following route works because route names are unique in this application.
+            await Shell.Current.GoToAsync($"beardetails?name={bearName}");
+            // The full route is shown below.
+            // await Shell.Current.GoToAsync($"//animals/domestic/cats/catdetails?name={catName}");
         }
     }
 }
--- XFShellDemo/pages/detailpages/BearDetailPage.xaml.cs
+++ XFShellDemo/pages/detailpages/BearDetailPage.xaml.cs
@@ -6,15 +6,39 @@ using System.Threading.Tasks;
 
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using XFShellDemo.Date;
 
 namespace XFShellDemo.pages.detailpages
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
+    [QueryProperty(nameof(Name), "name")]
     public partial class BearDetailPage : ContentPage
     {
+        public string Name
+        {
+            set
+            {
+                LoadAnimal(value);
+            }
+        }
+
         public BearDetailPage()
         {
             InitializeComponent();
         }
+
+        void LoadAnimal(string name)
+        {
+            try
+            {
+                Animal animal = BearData.Bears.FirstOrDefault(a => a.Name == name);
+                BindingContext = animal;
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Failed to load animal.");
+            }
+        }
     }
+
 }
